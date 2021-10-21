8698b55704a40e2444907d32a284da1f883b8203
unknown <mstacha@tamu.edu>
2021-10-21 12:21:06 -0500

    initial commit

---
+++ src/main/java/com/movieProjectAPI/Config/SpringJbdcConfig.java
@@ -0,0 +1,21 @@
+package com.movieProjectAPI.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@ComponentScan("com.movieProjectAPI.")
+public class SpringJbdcConfig {
+    @Bean
+    public DataSource mysqlDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/springjdbc");
+        dataSource.setUsername("guest_user");
+        dataSource.setPassword("guest_password");
+
+        return dataSource;
+    }
+}
